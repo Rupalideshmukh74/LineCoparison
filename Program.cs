47d90edd98b65979a068733b5580d2abece38a43
Rupalideshmukh74 <deshmukhrupali39@gmail.com>
2021-08-05 14:03:05 +0530

    [Rupali]Add:Welcome message

--- Program.cs
+++ Program.cs
@@ -4,11 +4,12 @@ namespace LineComparisonProblem
 {
     class Program
     {
-        
 
+      
         static void Main(string[] args)
         {
-            Console.WriteLine("welcome to line computation"); 
+            Console.WriteLine("welcome to Line comparison Problem");
         }
+           
     }
 }
